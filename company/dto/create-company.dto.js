d10379e3c40e9fa1824deae8acedced3a380e30b
Ritesh Saraf <ritesh.saraf327@gmail.com>
2026-06-06 12:39:34 +0530

    company folder changes

--- company/dto/create-company.dto.js
+++ company/dto/create-company.dto.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 module.exports = Joi.object({
   name: Joi.string().required(),
-  bannerImage: Joi.string().required(),
+  bannerImage: Joi.string(),
   description: Joi.string().required(),
   branches: Joi.string().required(),
   socialLinks: Joi.string().required()
